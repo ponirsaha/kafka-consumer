ca645e917151904f5fe150c2956e0821efb8b204
ponirsaha <ponir@live.com>
2023-11-02 21:27:44 +0600

    string to object type message

--- src/main/java/com/kafka/consumer/consumer/KafkaMessageListener.java
+++ src/main/java/com/kafka/consumer/consumer/KafkaMessageListener.java
@@ -1,14 +1,26 @@
 package com.kafka.consumer.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafka.consumer.model.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class KafkaMessageListener {
-    @KafkaListener(topics = "3rd-topic", groupId = "3rd-topic-group-1")
-    public void consume(String message) {
-        log.info("Consume the message =>> [{}]", message);
+    @KafkaListener(topics = "4th-topic", groupId = "3rd-topic-group-2")
+    public void consume(@Payload String message) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Customer customer = objectMapper.readValue(message, Customer.class);
+        if (customer.getAge() < 18) {
+            System.out.println("you are not authorized");
+        }
+        log.info("Consume the message =>> [{}]", customer);
     }
 }
---
+++ src/main/java/com/kafka/consumer/model/Customer.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private Integer age;
+}
